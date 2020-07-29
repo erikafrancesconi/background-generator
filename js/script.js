c09d3f27f562f5f1cec23de25c43f22df476a2e3
Erika Francesconi <fiorediren@gmail.com>
2020-07-29 21:32:45 +0200

    Decluttering

--- js/script.js
+++ js/script.js
@@ -1,39 +1,35 @@
-// TODO: 
-// replace querySelector with getElementById which is faster
-// replace firstChild.replaceWith with textContent which is more readable
-
 var body = document.querySelector('body');
 
-var startColorElement = document.querySelector('#startcolor');
-var endColorElement = document.querySelector('#endcolor');
-var startColor = startColorElement.firstChild.data;
-var endColor = endColorElement.firstChild.data;
+var startColorElement = document.getElementById('startcolor');
+var endColorElement = document.getElementById('endcolor');
+var startColor = startColorElement.textContent;
+var endColor = endColorElement.textContent;
 var originalStartColor = startColor;
 var originalEndColor = endColor;
 
-var direction1 = document.querySelector('#direction1');
-var direction2 = document.querySelector('#direction2');
+var direction1 = document.getElementById('direction1');
+var direction2 = document.getElementById('direction2');
 var direction = direction1.value;
 var originalDirection = direction;
 var directionElement = document.getElementById('dir');
 
 function redraw() {
-  var style = 'background: linear-gradient(' + direction + ', ' + startColor + ' , ' + endColor + ')';
-  body.setAttribute('style', style);
+  var style = 'linear-gradient(' + direction + ', ' + startColor + ' , ' + endColor + ')';
+  body.style.background = style;
 }
 
 function setColor(which, color) {
   if (which === 'start') {
     startColor = color;
-    startColorElement.firstChild.replaceWith(document.createTextNode(color));
+    startColorElement.textContent = color;
   } else if (which === 'end') {
     endColor = color;
-    endColorElement.firstChild.replaceWith(document.createTextNode(color));
+    endColorElement.textContent = color;
   }
 }
 
-var colora = document.querySelector('#stc');
-var colorb = document.querySelector('#endc');
+var colora = document.getElementById('stc');
+var colorb = document.getElementById('endc');
 colora.addEventListener('input', function() {
   setColor('start', this.value);
   redraw();
@@ -56,8 +52,7 @@ resetBtn.addEventListener('click', function() {
 
 function setDirection(value) {
   direction = value;
-  directionElement.firstChild.replaceWith(document.createTextNode(value));
-  // directionElement.textContent = value;
+  directionElement.textContent = value;
 }
 
 direction1.addEventListener('input', function() {
